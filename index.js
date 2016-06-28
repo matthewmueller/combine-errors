13978ee88d2a755706311de58f7bade028da730a
Matthew Mueller <mattmuelle@gmail.com>
2016-06-28 15:56:51 +0700

    use ES5 syntax

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
  * Module Dependencies
  */
 
-let Custom = require('custom-error-instance')
-let uniq = require('lodash.uniqby')
+var Custom = require('custom-error-instance')
+var uniq = require('lodash.uniqby')
 
 /**
  * Use a custom error type
  */
 
-let MultiError = Custom('MultiError')
+var MultiError = Custom('MultiError')
 
 /**
  * Export `Error`
@@ -26,16 +26,16 @@ module.exports = error
 function error (errors) {
   if (!(this instanceof error)) return new error(errors)
   errors = Array.isArray(errors) ? errors : [ errors ]
-  errors = uniq(errors, err => err.stack)
+  errors = uniq(errors, function (err) { return err.stack })
   if (errors.length === 1) return errors[0]
-  let multierror = new MultiError({
-    message: errors.map(err => err.message).join('; '),
-    errors: errors.reduce((errs, err) => errs.concat(err.errors || err), []),
+  var multierror = new MultiError({
+    message: errors.map(function (err) { return err.message }).join('; '),
+    errors: errors.reduce(function (errs, err) { return errs.concat(err.errors || err) }, []),
   })
 
   // lazily get/set the stack
   multierror.__defineGetter__('stack', function() {
-    return errors.map(err => err.stack).join('\n\n')
+    return errors.map(function (err) { return err.stack }).join('\n\n')
   })
 
   multierror.__defineSetter__('stack', function(value) {
